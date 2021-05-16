8195997f7fb67d0bcb4c1b0629707b737a216e98
Holger Wiesner (home) <holger.wiesner@transresult.de>
2021-05-16 07:44:49 +0200

    in again

--- choco.ps1
+++ choco.ps1
@@ -295,3 +295,7 @@ If ($args) {
 # Call the desired tweak functions
 $tweaks | ForEach { Invoke-Expression $_ }
 
+
+
+Write-Output "How to view installed apps with PowerShell on Windows 10..."
+Write-Output "Get-AppxPackage –AllUsers | Select Name, PackageFullName"
