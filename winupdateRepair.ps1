382cf3da0686d5d3dfdff6a1f4833a35d733e2ae
Holger@stagy <holger.wiesner@transresult.de>
2021-05-21 13:23:24 +0200

    fixing winupdateRepair

--- winupdateRepair.ps1
+++ winupdateRepair.ps1
@@ -1,5 +1,15 @@
-net stop wuauserv 
-net stop bits 
-rd /s /q %windir%\SoftwareDistribution\Download 
-net start wuauserv 
-net start bits wuauclt.exe /updatenow
+# net stop wuauserv 
+# net stop bits 
+# rd /s /q %windir%\SoftwareDistribution\Download 
+# net start wuauserv 
+# net start bits wuauclt.exe /updatenow
+
+Stop-Service wuauserv
+Stop-Service bits
+Remove-Item "$env:windir\SoftwareDistribution\Download" -Recurse # -ErrorAction SilentlyContinue
+Start-Service wuauserv 
+#net start bits wuauclt.exe /updatenow
+Start-Service bits 
+#Wuauclt /dectectnow /updatenow
+
+(New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow()
